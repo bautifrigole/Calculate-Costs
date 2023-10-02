540f726a7f820cb00d9b76ad94c8f0fd8ac3bfe3
Francisco Devaux <frandevaux2013@gmail.com>
2023-10-02 18:52:11 -0300

    Frontend directory initiated

---
+++ Frontend/calculate-costs/src/App.tsx
@@ -0,0 +1,19 @@
+import { Box, Text } from "@chakra-ui/react";
+function App() {
+  return (
+    <Box
+      height={"100vh"}
+      margin={0}
+      padding={0}
+      justifyContent={"center"}
+      alignItems={"center"}
+      display={"flex"}
+    >
+      <Text padding={0} margin={0}>
+        Calculate Costs papaaaaaaa
+      </Text>
+    </Box>
+  );
+}
+
+export default App;
